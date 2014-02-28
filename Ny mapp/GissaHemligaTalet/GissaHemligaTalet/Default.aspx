242b1e7749b1e81d836685a6a3fb1157fc5266ef
alexander27 <ad222in@student.lnu.se>
2014-02-28 20:36:11 +0100

    hemligaTalet

---
+++ Ny mapp/GissaHemligaTalet/GissaHemligaTalet/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GissaHemligaTalet.Default" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <h1>Gissa det hemliga talet</h1>
+        <div id="topbar"></div>
+
+        <asp:Label ID="Label1" runat="server" Text="Ange ett heltal mellan 1 och 100"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Textfältet får inte vara tomt!" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="RangeValidator" MaximumValue="99" MinimumValue="1" Display="Dynamic" ControlToValidate="TextBox1"></asp:RangeValidator>
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Ny mapp/GissaHemligaTalet/GissaHemligaTalet/Model/SecretNumber.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GissaHemligaTalet
+{                                                                           
+    public enum Outcome { Indefinite,Low,High,Correct,NoMoreGuesses,PreviousGuesses};
+
+    public class SecretNumber
+    {
+        private int _number;
+        private List<int> _previousGuesses;
+
+        public const int MaxNumberOfGuesses = 7;
+
+        public bool CanMakeGuess
+        {
+            get
+            {
+                return Count < MaxNumberOfGuesses && !_previousGuesses.Contains(_number);
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return _previousGuesses.Count();
+            }
+        }
+        public int Number
+        {
+            get
+            {
+                if (CanMakeGuess)
+                {
+                    return null;
+                }
+                return _number;
+            }
+        }
+
+        public Outcome Outcome { get; private set; }
+
+        public ReadOnlyCollection <int> PreviousGuesses
+        {
+            get
+            {
+                return _previousGuesses.AsReadOnly();
+            }
+
+        }
+        public void Initalize()
+        {
+            Random Random = new Random();
+            _number = Random.Next(1, 100);
+
+            _previousGuesses.Clear();
+
+            Outcome = Outcome.Indefinite;
+        }
+        public Outcome MakeGuess(int guess)
+        {
+            
+            if (guess < 1 || guess > 100)
+            
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            
+            if (Count == MaxNumberOfGuesses)
+ 
+            {
+                return Outcome = Outcome.NoMoreGuesses;
+            }
+
+
+            if (_previousGuesses.Contains(guess))
+            {
+                return Outcome = Outcome.PreviousGuesses;
+            }
+            _previousGuesses.Add(guess);
+
+            if (guess < _number)
+            {
+                return Outcome = Outcome.Low;
+            }
+
+            if (guess > _number)
+            {
+                return Outcome = Outcome.High;
+            }
+
+            else 
+            {
+                return Outcome = Outcome.Correct;
+            }
+        }
+        
+        public SecretNumber() 
+        { 
+            _previousGuesses =new List<int>(MaxNumberOfGuesses);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
